cae35003eee0d4aa1df1ab6272bf903b6b59986b
ganirockz <ganeshrapeti8@gmail.com>
2020-11-10 19:47:05 +0530

    [Ganesh]Added: pincode validation first rule

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,6 +3,7 @@ class EmployeePayrollData{
     salary;
     gender;
     startDate;
+    pin;
 
     constructor(...params){
         let idAndSalaryRegex = RegExp("[0-9]+");
@@ -23,11 +24,16 @@ class EmployeePayrollData{
         else{
             throw "Gender is incorrect";
         }
-        let dateRegex = RegExp("[0-2][0-9][0-9][0-9][-]");
         if(params[4] <= new Date()){
         this.startDate = params[4];}
         else{
             throw "Date is incorrect";
+        }
+        let pincodeRegex = RegExp("^[1-9][0-9]{5}$");
+        if(pincodeRegex.test(params[5])){
+            this.pin = params[5];
+        }else{
+            throw "Invalid Pin code";
         }
     }
 
@@ -48,13 +54,6 @@ class EmployeePayrollData{
     }
 }
 
-/*let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
-console.log(employeePayrollData.toString());
-try{
-employeePayrollData.name = "john";
-console.log(employeePayrollData.toString());
-}catch(e){
-    console.error(e);
-}*/
-let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
+let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date(),400088);
 console.log(newEmployeePayrollData);
+
